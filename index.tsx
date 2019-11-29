630901d2f43c5383c0b20079f8cfa9927061d692
AnjaliMusmade <anjalimusmade29@gmail.com>
2019-11-29 12:59:52 +0530

    2 files modified, 2 added

--- index.tsx
+++ index.tsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
-let data=
-{
-  todos:['','','']
-};
+// let data=
+// {
+//   todos:[{},{},{}],
+//   filters:
+// };
+import {createStore,combineReducers} from 'redux';
+import todosReducer from './reducers/todoReducer';
+import filterReducer from './reducers/filterReducer';
+
+
+
+let store=createStore(combineReducers({todos: todosReducer,filter: filterReducer}));
+
+store.subscribe(()=>console.log(console.log(store.getState())));
+
+store.dispatch({type:"ADD_TODO",text:"Onion"});
+store.dispatch({type:"ADD_TODO",text:"Pen"});
+store.dispatch({type:"ADD_TODO",text:"Milk"});
+store.dispatch({type:"TOGGLE_TODO",text:"Onion"});
